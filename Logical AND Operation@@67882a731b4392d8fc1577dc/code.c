be5147d0eaa8dcf43b9904abfd4a7b33d3afc239
RG1477 <raghav0233.becse24@chitkara.edu.in>
2025-02-06 11:17:58 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -4,7 +4,7 @@ int main() {
     int num1, num2;
     
     // Input two numbers
-    printf("Enter two numbers: ");
+    
     scanf("%d %d", &num1, &num2);
     
     // Check if both numbers are greater than zero
