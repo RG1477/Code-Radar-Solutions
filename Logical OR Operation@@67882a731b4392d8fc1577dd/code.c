0fa12789da7fee28e1a8241cb3119ff0d9e14241
RG1477 <raghav0233.becse24@chitkara.edu.in>
2025-02-06 11:22:15 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -9,9 +9,9 @@ int main() {
     
     // Check if at least one number is greater than zero
     if (num1 > 0 || num2 > 0) {
-        printf("true\n");
+        printf("True\n");
     } else {
-        printf("false\n");
+        printf("False\n");
     }
     
     return 0;
