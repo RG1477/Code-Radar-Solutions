86e653869d94c487a51db00d79a6876b7145e8c2
RG1477 <raghav0233.becse24@chitkara.edu.in>
2025-02-10 14:07:17 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     double num1, num2, result;
     char op;
+    int intNum1, intNum2;  // For modulus operation
 
-    // Taking input: first number, operator, second number
-    printf("Enter an expression (e.g., 5 + 3): ");
-    if (scanf("%lf %c %lf", &num1, &op, &num2) != 3) {
-        printf("Invalid input\n");
-        return 1;
+    while (1) {
+        printf("\nEnter an expression (e.g., 5 + 3) or 'q' to quit: ");
+        
+        if (scanf("%lf %c %lf", &num1, &op, &num2) != 3) {
+            printf("Exiting...\n");
+            break; // Exit loop if input is invalid
+        }
+
+        switch (op) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': 
+                if (num2 != 0) 
+                    result = num1 / num2;
+                else {
+                    printf("Error: Division by zero\n");
+                    continue;
+                }
+                break;
+            case '%': // Handling modulus (only for integers)
+                intNum1 = (int)num1;
+                intNum2 = (int)num2;
+                if (intNum2 != 0)
+                    result = intNum1 % intNum2;
+                else {
+                    printf("Error: Modulo by zero\n");
+                    continue;
+                }
+                break;
+            case '^': result = pow(num1, num2); break; // Exponentiation
+            default: 
+                printf("Error: Invalid operator\n");
+                continue;
+        }
+
+        printf("Result: %.2lf\n", result);
     }
-
-    // Performing the operation based on the operator
-    switch (op) {
-        case '+': result = num1 + num2; break;
-        case '-': result = num1 - num2; break;
-        case '*': result = num1 * num2; break;
-        case '/': 
-            if (num2 != 0) 
-                result = num1 / num2;
-            else {
-                printf("Error: Division by zero\n");
-                return 1;
-            }
-            break;
-        default: 
-            printf("Error: Invalid operator\n");
-            return 1;
-    }
-
-    // Printing the result
-    printf("Result: %.2lf\n", result);
 
     return 0;
 }
