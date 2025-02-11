cc46dc4b714b1ce81727b454c474428036c9eddf
RG1477 <raghav0233.becse24@chitkara.edu.in>
2025-02-11 14:08:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
 
 int main() {
+    char operator;
     double num1, num2, result;
-    char op;
-
-    // Taking input from the user
-    printf("Enter an expression (e.g., 5 + 3): ");
-    if (scanf("%lf %c %lf", &num1, &op, &num2) != 3) {
-        printf("Error: Invalid input format.\n");
-        return 1;
-    }
-
-    // Performing the operation based on the operator
-    switch (op) {
-        case '+': 
-            result = num1 + num2; 
+    scanf("%lf %lf", &num1, &num2);
+    scanf(" %c", &operator);
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
             break;
-        case '-': 
-            result = num1 - num2; 
+        case '-':
+            result = num1 - num2;
             break;
-        case '*': 
-            result = num1 * num2; 
+        case '*':
+            result = num1 * num2;
             break;
-        case '/': 
-            if (num2 != 0) 
+        case '/':
+            if (num2 != 0) {
                 result = num1 / num2;
-            else {
-                printf("Error: Division by zero is not allowed.\n");
+            } else {
+                printf("error\n");
                 return 1;
             }
             break;
-        default: 
-            printf("Error: Invalid operator. Use +, -, *, or /.\n");
+        default:
+            printf("Invalid operator.\n");
             return 1;
     }
 
-    // Printing the result
-    printf("Result: %.2lf\n", result);
-
-    return 0;
-}
+    printf("%.f\n", result);
