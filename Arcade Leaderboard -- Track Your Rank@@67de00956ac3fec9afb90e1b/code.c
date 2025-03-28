370094b508f065bb35189dc8cb882e009f5d7ebd
RG1477 <raghav0233.becse24@chitkara.edu.in>
2025-03-28 11:22:12 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void computeRanks(int leaderboard[], int n, int playerScores[], int m) {
+    int ranks[n], rank = 1;
+
+    // Assign ranks to leaderboard (Dense Ranking)
+    ranks[0] = rank;
+    for (int i = 1; i < n; i++) {
+        if (leaderboard[i] == leaderboard[i - 1])
+            ranks[i] = rank;
+        else
+            ranks[i] = ++rank;
+    }
+
+    int index = n - 1; // Start from the lowest rank in the leaderboard
+
+    // Process each player's score
+    for (int i = 0; i < m; i++) {
+        while (index >= 0 && playerScores[i] >= leaderboard[index])
+            index--; // Move up in rank
+        
+        printf("%d\n", index == -1 ? 1 : ranks[index] + 1);
+    }
+}
+
+int main() {
+    int n, m;
+    
+    // Read leaderboard size and scores
+    scanf("%d", &n);
+    int leaderboard[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &leaderboard[i]);
+
+    // Read number of games and player scores
+    scanf("%d", &m);
+    int playerScores[m];
+    for (int i = 0; i < m; i++)
+        scanf("%d", &playerScores[i]);
+
+    // Compute ranks after each game
+    computeRanks(leaderboard, n, playerScores, m);
+
+    return 0;
+}
